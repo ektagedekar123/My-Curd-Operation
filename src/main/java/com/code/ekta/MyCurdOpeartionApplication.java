e3b0efa15d429eaee2f697e32c044003cb94dc44
ektagedekar123 <gedekarekta123@gmail.com>
2023-08-27 13:17:57 -0700

    First commited

---
+++ src/main/java/com/code/ekta/MyCurdOpeartionApplication.java
@@ -0,0 +1,13 @@
+package com.code.ekta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCurdOpeartionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCurdOpeartionApplication.class, args);
+	}
+
+}
